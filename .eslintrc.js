ae795d78819f298c45bd263a05a8c5fff5e2ad3d
Darien Lombardi <darienlombardi@DLombardi.lan>
2024-11-03 18:18:26 -0500

    update eslint and tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
+  // extends: ['./packages/config-eslint/react'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
