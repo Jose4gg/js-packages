60c63b5d100bf8805710d6c6fd78d9b3d8f308d5
Darien Lombardi <darienlombardi@DLombardinayya16MBP.local>
2024-11-05 15:16:53 -0500

    update publish

---
+++ .github/check-package-changed.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if a package name argument is provided
+if [ -z "$1" ]; then
+  echo "Please provide a package name as an argument."
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  comparison_branch="origin/main"
+else
+  comparison_branch="$2"
+fi
+
+# Store the package name argument
+package_name=$1
+
+# Run Turbo with --dry-run and capture the output
+output=$(pnpm turbo run lint --filter="[$comparison_branch..]" --dry-run)
+
+# Check if the specified package name appears in the output
+if echo "$output" | grep -q -E "^$package_name(\s|#)"; then
+  echo "true"
+else
+  echo "false"
+fi
---
+++ packages/tsconfig/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @nayya-com/tsconfig
+
+## 0.1.0
+
+### Minor Changes
+
+- foo
