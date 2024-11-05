2b8fed98fe93e81d73b73b27ea227aad6ce5bbba
Darien Lombardi <darienlombardi@DLombardinayya16MBP.local>
2024-11-05 15:42:10 -0500

    test publish

--- .github/check-package-changed.sh
+++ .github/check-package-changed.sh
@@ -7,16 +7,16 @@ if [ -z "$1" ]; then
 fi
 
 if [ -z "$2" ]; then
-  comparison_branch="origin/main"
-else
-  comparison_branch="$2"
+  echo "Please provide a comparison branch as an argument."
+  exit 1
 fi
+
 
 # Store the package name argument
 package_name=$1
-
+comparison_branch=$2
 # Run Turbo with --dry-run and capture the output
-output=$(pnpm turbo run lint --filter="[$comparison_branch..]" --dry-run)
+output=$(pnpm turbo run lint --filter="...[$comparison_branch]" --dry-run)
 
 # Check if the specified package name appears in the output
 if echo "$output" | grep -q -E "^$package_name(\s|#)"; then
