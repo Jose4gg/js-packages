e7a95deedfb821fd12deadef55aa78e1cb7efc7c
Darien Lombardi <darienlombardi@DLombardi.attlocal.net>
2024-11-25 11:50:11 -0500

    update tsconfigs for ui components to include baseURL

--- packages/ui/src/components/date-input/rollup.config.mjs
+++ packages/ui/src/components/date-input/rollup.config.mjs
@@ -1,43 +1,42 @@
-import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
 import dts from 'rollup-plugin-dts';
+
 export default [
   {
-    input: ['src/index.ts'],
+    input: 'src/index.ts',
     output: [
-      {
-        file: 'lib/index.mjs',
-        format: 'esm',
-        sourcemap: true,
-        exports: 'named',
-        globals: {},
-      },
       {
         file: 'lib/index.js',
         format: 'cjs',
         sourcemap: true,
-        exports: 'named',
-        globals: {},
+      },
+      {
+        file: 'lib/index.mjs',
+        format: 'esm',
+        sourcemap: true,
       },
     ],
+    external: [
+      'react',
+      'react-dom',
+      'styled-components',
+      '@nayya-com/wardrobe',
+      '@nayya-com/text-input',
+    ],
     plugins: [
-      resolve({
-        preferBuiltins: true,
-      }),
+      resolve(),
       commonjs(),
       typescript({
         tsconfig: './tsconfig.json',
+        clean: true,
       }),
     ],
-    external: ['styled-components', 'react', 'react/jsx-runtime'],
   },
   {
     input: 'src/index.ts',
-    output: {
-      file: 'lib/index.d.ts',
-      format: 'es',
-    },
+    output: [{ file: 'lib/index.d.ts', format: 'esm' }],
     plugins: [dts()],
   },
 ];
