c26aba9debce736a9643583429dcefd4a4f82b76
Darien Lombardi <darienlombardi@DLombardinayya16MBP.local>
2024-12-04 17:02:40 -0500

    fix react-select typescript definitions for autocomplete and select-input so that tests and storybooks don't fail TS checks

--- packages/ui/src/components/autocomplete/src/types.ts
+++ packages/ui/src/components/autocomplete/src/types.ts
@@ -1,5 +1,5 @@
 import { Theme } from '@nayya-com/wardrobe';
-import { GroupBase, Props, SelectInstance } from 'react-select';
+import { OptionsOrGroups, GroupBase, Props, SelectInstance } from 'react-select';
 import 'styled-components';
 
 export type StyledAutocompleteProps = AutocompleteProps & {
@@ -9,13 +9,15 @@ export type StyledAutocompleteProps = AutocompleteProps & {
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme {}
 }
+export interface Option {
+  label: string;
+  value: string | number;
+}
 
-export interface AutocompleteProps<
-  Option = unknown,
-  IsMulti extends boolean = false,
-  Group extends GroupBase<Option> = GroupBase<Option>,
-> extends Props<Option, IsMulti, Group> {
-  searchFn: (inputValue: string) => Promise<Option[]>;
+export interface AutocompleteProps extends Props {
+  searchFn: (
+    inputValue: string,
+  ) => Promise<OptionsOrGroups<typeof Option, GroupBase<typeof Option>>>;
   error?: boolean;
   helperText?: string;
   testId?: string;
@@ -24,7 +26,7 @@ export interface AutocompleteProps<
   inputActionHandler?: () => void;
   Input?: () => void;
   header?: string;
-  ref?: React.Ref<SelectInstance<Option, IsMulti, Group>>;
+  ref?: React.Ref<SelectInstance>;
 }
 
 export type { SelectInstance as AutocompleteInstance };
--- packages/ui/src/components/select-input/src/types.ts
+++ packages/ui/src/components/select-input/src/types.ts
@@ -1,17 +1,13 @@
 import { Theme } from '@nayya-com/wardrobe';
 import { ReactNode } from 'react';
-import type { GroupBase, Props, SelectInstance } from 'react-select';
+import type { Props, SelectInstance } from 'react-select';
 import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme {}
 }
 
-export interface SelectInputProps<
-  Option = unknown,
-  IsMulti extends boolean = false,
-  Group extends GroupBase<Option> = GroupBase<Option>,
-> extends Props<Option, IsMulti, Group> {
+export interface SelectInputProps extends Props {
   error?: boolean;
   helperText?: string;
   warning?: boolean;
@@ -19,7 +15,7 @@ export interface SelectInputProps<
   id: string;
   fullWidth?: boolean;
   visibleInput?: boolean;
-  ref?: React.Ref<SelectInstance<Option, IsMulti, Group>>;
+  ref?: React.Ref<SelectInstance>;
 }
 
 export type { SelectInstance };
